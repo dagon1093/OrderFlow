143aedb27568fde15316e2c0baf7ca8c7cafe56e
dagon1093 <dagon1093@gmail.com>
2026-04-30 02:01:24 +0300

    some fixes

--- src/OrderFlow.Contracts/OrderCreatedEvent.cs
+++ src/OrderFlow.Contracts/OrderCreatedEvent.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OrderFlow.Contracts
+﻿namespace OrderFlow.Contracts
 {
     public sealed record OrderCreatedEvent(
         Guid OrderId,
