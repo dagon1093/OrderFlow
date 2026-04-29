5e75f7f49fc30b298666dc0fc542d817be970d51
dagon1093 <dagon1093@gmail.com>
2026-04-30 01:31:44 +0300

    add first producer

---
+++ src/OrderFlow.Contracts/OrderCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderFlow.Contracts
+{
+    public sealed record OrderCreatedEvent(
+        Guid OrderId,
+        DateTimeOffset CreatedAt,
+        decimal Amount,
+        string Currency
+        );
+}
--- src/OrderFlow.PaymentService/Program.cs
+++ src/OrderFlow.PaymentService/Program.cs
@@ -1,6 +1,7 @@
 using OrderFlow.PaymentService;
 
 var builder = Host.CreateApplicationBuilder(args);
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
