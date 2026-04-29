37a4bfb93b3ae0da7b7233b78b17a28a1da7c510
dagon1093 <dagon1093@gmail.com>
2026-04-30 02:27:35 +0300

    contract updated

--- src/OrderFlow.Contracts/OrderCreatedEvent.cs
+++ src/OrderFlow.Contracts/OrderCreatedEvent.cs
@@ -1,7 +1,9 @@
 ﻿namespace OrderFlow.Contracts
 {
     public sealed record OrderCreatedEvent(
+        Guid EventId,
         Guid OrderId,
+        Guid UserId,
         DateTimeOffset CreatedAt,
         decimal Amount,
         string Currency
